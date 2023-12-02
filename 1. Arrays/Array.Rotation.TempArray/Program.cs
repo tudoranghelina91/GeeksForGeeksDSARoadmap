cc7f063bbf1e1389ccfcaa005d977c290f1b0eae
Tudor Anghelina <tudor.anghelina91@gmail.com>
2023-12-02 22:59:51 +0200

    Add 2 algos for rotating arrays

---
+++ 1. Arrays/Array.Rotation.TempArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using Array.Utils;
+
+int[] arr = ArrayGenerator.GenerateRandom();
+arr.Print();
+
+Console.WriteLine("Input the desired rotations to be performed on the array:");
+
+int numberOfRotations = Convert.ToInt32(Console.ReadLine());
+
+int[] temp =  new int[arr.Length];
+
+int k = 0;
+
+for (int i = arr.Length - numberOfRotations; i < arr.Length; i++)
+{
+    temp[k++] = arr[i];
+}
+
+for (int i = 0; i < arr.Length - numberOfRotations; i++)
+{
+    temp[k++] = arr[i];
+}
+
+temp.Print();
---
+++ 1. Arrays/Arrays.Rotation.OneByOne/Program.cs
@@ -0,0 +1,22 @@
+﻿using Array.Utils;
+
+int[] arr = ArrayGenerator.GenerateRandom();
+arr.Print();
+
+Console.WriteLine("Input the desired rotations to be performed on the array:");
+
+int numberOfRotations = Convert.ToInt32(Console.ReadLine());
+
+for (int rotation = 1; rotation <= numberOfRotations; rotation++)
+{
+    int carry = arr[arr.Length - 1];
+
+    for (int i = arr.Length - 1; i >= 1; i--)
+    {
+        arr[i] = arr[i - 1];
+    }
+
+    arr[0] = carry;
+}
+Console.WriteLine("Arrat after rotation:");
+arr.Print();
