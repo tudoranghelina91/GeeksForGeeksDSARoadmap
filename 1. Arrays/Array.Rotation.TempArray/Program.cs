83f3e20a477c447e8b265fac8db272c787d320a4
Tudor Anghelina <tudor.anghelina91@gmail.com>
2023-12-03 16:49:12 +0200

    Remove edge case

--- 1. Arrays/Array.Rotation.TempArray/Program.cs
+++ 1. Arrays/Array.Rotation.TempArray/Program.cs
@@ -1,11 +1,11 @@
 ﻿using Array.Utils;
-
+// Problem with this approach is that the number of rotations can't be greater than the length of the array
 int[] arr = ArrayGenerator.GenerateRandom();
 arr.Print();
 
 Console.WriteLine("Input the desired rotations to be performed on the array:");
 
-int numberOfRotations = Math.Abs(Convert.ToInt32(Console.ReadLine()) - arr.Length);
+int numberOfRotations = Convert.ToInt32(Console.ReadLine());
 
 int[] temp =  new int[arr.Length];
 
