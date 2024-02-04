d4468b7f4dd1124a739a731dc79b75a5a69e03f2
Tudor Anghelina <16164903+tudoranghelina91@users.noreply.github.com>
2024-02-04 22:47:36 +0200

    Implemented binary search

--- 1. Arrays/Arrays.Sorted.Search/Program.cs
+++ 1. Arrays/Arrays.Sorted.Search/Program.cs
@@ -1,3 +1,33 @@
 ﻿using Array.Utils;
+int[] arr = ArrayGenerator.GenerateSequential(10, 5);
 
-// TODO: Implement Binary Search Again
+arr.Print();
+
+int key = Convert.ToInt32(Console.ReadLine());
+
+int start = 0;
+int end = arr.Length - 1;
+int mid = (start + end) / 2;
+
+while (start <= end)
+{
+    if (arr[mid] == key)
+    {
+        Console.WriteLine($"Element {key} found at position {mid}");
+        return;
+    }
+
+    if (key < arr[mid])
+    {
+        end = mid - 1;
+    }
+
+    if (key > arr[mid])
+    {
+        start = mid + 1;
+    }
+
+    mid = (start + end) / 2;
+}
+
+Console.WriteLine($"Element {key} not found");
