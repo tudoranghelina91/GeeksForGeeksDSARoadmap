e9a390da5c8a65314a3c353a2ce27565a5ad2bd2
Tudor Anghelina <16164903+tudoranghelina91@users.noreply.github.com>
2024-02-18 14:18:38 +0200

    Add sorting algorithms

---
+++ 2. Sorting/Sorting/Sorting.BubbleSort/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Array.Utils;
+
+var arr = ArrayGenerator.GenerateRandom(20);
+arr.Print();
+
+bool sorted;
+
+do
+{
+    sorted = true;
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            sorted = false;
+            arr[i] += arr[i + 1];
+            arr[i + 1] = arr[i] - arr[i + 1];
+            arr[i] -= arr[i + 1];
+        }
+    }
+} while (!sorted);
+
+arr.Print();
---
+++ 2. Sorting/Sorting/Sorting.SelectionSort/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Array.Utils;
+
+Console.WriteLine("Hello, World!");
+
+var arr = ArrayGenerator.GenerateRandom();
+
+arr.Print();
+
+for (int i = 0; i < arr.Length - 1; i++)
+{
+    for (int j = i + 1; j < arr.Length; j++)
+    {
+        if (arr[i] > arr[j])
+        {
+            arr[i] += arr[j];
+            arr[j] = arr[i] - arr[j];
+            arr[i] -= arr[j];
+        }
+    }
+}
+
+arr.Print();
