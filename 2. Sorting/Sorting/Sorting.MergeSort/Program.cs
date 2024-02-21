24b4119aaedb4922944888bfbb5bc57a6dd04e34
Tudor Anghelina <16164903+tudoranghelina91@users.noreply.github.com>
2024-02-21 22:32:40 +0200

    Update sorting algorithm

--- 2. Sorting/Sorting/Sorting.MergeSort/Program.cs
+++ 2. Sorting/Sorting/Sorting.MergeSort/Program.cs
@@ -1,4 +1,26 @@
 ﻿using Array.Utils;
 
-var arr = ArrayGenerator.GenerateRandom(20);
+void MergeSort(int[] arr, int start, int end)
+{
+    int mid = (start + end) / 2 + (start + end) % 2;
+    
+    MergeSort(arr, start, mid);
+    MergeSort(arr, mid, end);
+
+    int i = start;
+    int j = mid;
+    int k = 0;
+
+    // TODO:
+
+    // 1. Copy data to temp arrays
+
+    // 2. Merge the two halves in sorted order
+}
+
+var arr = ArrayGenerator.GenerateRandom(10);
+arr.Print();
+
+MergeSort(arr, 0, arr.Length);
+
 arr.Print();
