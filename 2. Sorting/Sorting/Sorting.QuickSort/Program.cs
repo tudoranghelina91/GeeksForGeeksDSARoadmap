915502f57b39f79495ff483b0a393d35ce361550
Tudor Anghelina <16164903+tudoranghelina91@users.noreply.github.com>
2024-03-17 22:48:16 +0200

    Add quicksort

---
+++ 2. Sorting/Sorting/Sorting.QuickSort/Program.cs
@@ -0,0 +1,47 @@
+﻿using Array.Utils;
+
+void QuickSort(int[] arr, int start, int end)
+{
+    if (start >= end)
+    {
+        return;
+    }
+
+    int pivot = end;
+
+    int i = start - 1;
+
+    for (int j = start; j < end; j++)
+    {
+        if (arr[j] < arr[pivot])
+        {
+            i++;
+            SwapElements(arr, i, j);
+        }
+    }
+
+    i++;
+    SwapElements(arr, i, end);
+
+    pivot = i;
+
+    QuickSort(arr, start, pivot - 1);
+    QuickSort(arr, pivot + 1, end);
+}
+
+static void SwapElements(int[] arr, int i, int j)
+{
+    if (i == j)
+    {
+        return;
+    }
+
+    arr[i] += arr[j];
+    arr[j] = arr[i] - arr[j];
+    arr[i] -= arr[j];
+}
+
+var arr = new int[] { 8, 2, 4, 7, 1, 3, 9, 6, 5 };
+arr.Print();
+QuickSort(arr, 0, arr.Length - 1);
+arr.Print();
