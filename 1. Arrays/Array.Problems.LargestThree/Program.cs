4f04cb9188fa2b7ad3a5399787974610669e0ca3
Tudor Anghelina <16164903+tudoranghelina91@users.noreply.github.com>
2024-05-22 21:41:16 +0300

    Add largest three problem

---
+++ 1. Arrays/Array.Problems.LargestThree/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Input: arr[] = { 10, 4, 3, 50, 23, 90 }
+//Output: 90, 50, 23
+
+//int[] arr = { 10, 4, 3, 50, 23, 90 };
+
+//Input: arr[] = { 6, 8, 1, 9, 2, 1, 10, 10 }
+//Output: 10, 9, 10
+
+//Input: arr[] = { 12, 13, 1, 10, 34, 1 }
+//Output: 34, 13, 12
+int[] arr = { 12, 13, 1, 10, 34, 1 };
+
+int first = 0, second = 0, third = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > first)
+    {
+        third = second;
+        second = first;
+        first = arr[i];
+        continue;
+    }
+
+    if (arr[i] > second && arr[i] != first)
+    {
+        third = second;
+        second = arr[i];
+        continue;
+    }
+
+    if (arr[i] > third && arr[i] != second)
+    {
+        third = arr[i];
+        continue;
+    }
+}
+
+Console.WriteLine($"{first} {second} {third}");
