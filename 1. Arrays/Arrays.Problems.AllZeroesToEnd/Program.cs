37f9a94a9e6b7a6c405e937fdab3e92e58ce3148
Tudor Anghelina <16164903+tudoranghelina91@users.noreply.github.com>
2024-05-27 18:29:14 +0300

    Add ZeroesToEnd

---
+++ 1. Arrays/Arrays.Problems.AllZeroesToEnd/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Move all zeroes to end of array
+//Last Updated : 18 Sep, 2023
+//Given an array of random numbers, Push all the zero’s of a given array to the end of the array. For example, if the given arrays is {1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0}, it should be changed to {1, 9, 8, 4, 2, 7, 6, 0, 0, 0, 0}. The order of all other elements should be same. Expected time complexity is O(n) and extra space is O(1).
+
+//Example:
+
+//Input: arr[] = { 1, 2, 0, 4, 3, 0, 5, 0 };
+//Output: arr[] = { 1, 2, 4, 3, 5, 0, 0, 0 };
+
+//Input: arr[]  = { 1, 2, 0, 0, 0, 3, 6 };
+//Output: arr[] = { 1, 2, 3, 6, 0, 0, 0 };
+
+//Input: arr[] = { 1, 2, 0, 0, 3, 6, 0 };
+
+int[] arr = { 1, 2, 0, 4, 3, 0, 5, 0 };
+bool allZeroesAtEnd;
+int end = arr.Length;
+
+do
+{
+    allZeroesAtEnd = true;
+    for (int i = 0; i < end; i++)
+    {
+        if (arr[i] == 0)
+        {
+            for (int j = i; j < arr.Length - 1; j++)
+            {
+                arr[j] += arr[j + 1];
+                arr[j + 1] = arr[j] - arr[j + 1];
+                arr[j] -= arr[j + 1];
+            }
+            end--;
+            allZeroesAtEnd = false;
+        }
+    }
+} while (!allZeroesAtEnd);
+
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.Write(arr[i] + " ");
+}
