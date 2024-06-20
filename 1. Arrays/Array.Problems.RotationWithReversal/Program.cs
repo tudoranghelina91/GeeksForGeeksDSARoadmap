8570264b15c2f99986b1e3eeb01f82d3d5b7b340
Tudor Anghelina <16164903+tudoranghelina91@users.noreply.github.com>
2024-06-20 19:02:35 +0300

    Add more problems

---
+++ 1. Arrays/Array.Problems.RotationWithReversal/Program.cs
@@ -0,0 +1,64 @@
+﻿// C# program for reversal algorithm
+// of array rotation
+using System;
+
+namespace Array.Problems.RotationWithReversal
+{
+    internal class Program
+    {
+        /* Function to left rotate arr[]
+        of size n by d */
+
+        private static void leftRotate(int[] arr, int d)
+        {
+            if (d == 0)
+                return;
+            var n = arr.Length;
+            // in case the rotating factor is
+            // greater than array length
+            d = d % n;
+            reverseArray(arr, 0, d - 1);
+            reverseArray(arr, d, n - 1);
+            reverseArray(arr, 0, n - 1);
+        }
+
+        /* Function to reverse arr[] from
+        index start to end*/
+
+        private static void reverseArray(int[] arr, int start,
+                                int end)
+        {
+            int temp;
+            while (start < end)
+            {
+                temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+        }
+
+        /*UTILITY FUNCTIONS*/
+        /* function to print an array */
+
+        private static void printArray(int[] arr)
+        {
+            for (var i = 0; i < arr.Length; i++)
+                Console.Write(arr[i] + " ");
+        }
+
+        // Driver code
+        public static void Main()
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+            var n = arr.Length;
+            var d = 2;
+
+            leftRotate(arr, d); // Rotate array by 2
+            printArray(arr);
+        }
+    }
+}
+
+// This code is contributed by Sam007
