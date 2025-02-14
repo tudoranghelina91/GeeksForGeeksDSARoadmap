026bcd9abe2f561a5656d85db1707d695e0ac2dc
Tudor Anghelina <16164903+tudoranghelina91@users.noreply.github.com>
2025-02-14 22:07:10 +0200

    Add DNF algo for Sort012 problem

---
+++ 1. Arrays/Sort012DNF.py
@@ -0,0 +1,18 @@
+# Dutch National Flag
+def sort012DNF(arr):
+    low, mid, high = 0, 0, len(arr)  - 1
+
+    while mid <= high:
+        if arr[mid] == 0:
+            arr[low], arr[mid] = arr[mid], arr[low]
+            low += 1
+            mid += 1
+        elif arr[mid] == 1:
+            mid += 1
+        else:
+            arr[mid], arr[high] = arr[high], arr[mid]
+            high -= 1
+
+arr = [0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1]
+sort012DNF(arr)
+print(arr)
