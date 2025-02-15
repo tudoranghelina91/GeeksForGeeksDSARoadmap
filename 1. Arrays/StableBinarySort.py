f5905d9e98bee0b82ff415f81f05819e3d5213f3
Tudor Anghelina <16164903+tudoranghelina91@users.noreply.github.com>
2025-02-15 23:35:48 +0200

    Add stable binary sort

---
+++ 1. Arrays/StableBinarySort.py
@@ -0,0 +1,16 @@
+def stableBinarySortNaive(arr):
+    even = [x for x in arr if x % 2 == 0]   
+    odd = [x for x in arr if x % 2 != 0]
+    return even + odd
+
+def stableBinarySortLomuto(arr):
+    i = -1
+    for j in range(len(arr)):
+        if arr[j] % 2 == 0:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    return arr
+
+print(stableBinarySortNaive([-5, -2, 0, 4, 7, 9]))
+print(stableBinarySortLomuto([-5, -2, 0, 4, 7, 9]))
