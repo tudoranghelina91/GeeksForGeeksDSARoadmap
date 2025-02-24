049446c3c33960f617af31832b1da87cda75165e
Tudor Anghelina <16164903+tudoranghelina91@users.noreply.github.com>
2025-02-24 15:24:16 +0200

    Remove comment

--- 4. Linked Lists/PrintCircular.py
+++ 4. Linked Lists/PrintCircular.py
@@ -8,6 +8,5 @@ def printList(head: Node):
         print(t.data, end= " ")
         t = t.next
 
-# TODO: Update list to support circulars
 list = LinkedList([1, 2, 3, 4, 5], True)
 printList(list.head)
