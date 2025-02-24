baeb6db84d5f52aa2eda16b2289c9d4a45b7d792
Tudor Anghelina <16164903+tudoranghelina91@users.noreply.github.com>
2025-02-24 21:52:27 +0200

    rollback

--- 1. Arrays/MaxProfitFromBuySellStock.py
+++ 1. Arrays/MaxProfitFromBuySellStock.py
@@ -1,12 +1,10 @@
 def maximumProfit(prices) -> int:
-    minimum = prices[0]
-    profit = 0
-
-    for i in range(len(prices)):
-        profit = max(profit, prices[i] - minimum)
-        minimum = min(minimum, prices[i])
-
-    return profit
+    sum = 0
+    for i in range(1, len(prices)):
+        if prices[i] > prices[i - 1]:
+            sum += prices[i] - prices[i - 1]
+    
+    return sum
 
 arr = [100, 180, 260, 310, 40, 535, 695]
 print(maximumProfit(arr))
