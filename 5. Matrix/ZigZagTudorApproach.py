bfb9e6c71c0bf2df7be405aa7b248fdbb5a5321d
Tudor Anghelina <16164903+tudoranghelina91@users.noreply.github.com>
2025-02-26 20:46:58 +0200

    Add Tudor approach for zig zag

---
+++ 5. Matrix/ZigZagTudorApproach.py
@@ -0,0 +1,36 @@
+# Can go left 1 step max
+# Can go down 1 step max
+# Can go diagonally left until a margin is encountered
+# Can go diagonally right until a margin is encountered
+
+# L = 1, R = 0, D = 0, U = 0
+
+# 1. Go left 1 step
+# 2. Set go left false
+# 3. Set go down true
+# 4. Set go right true
+
+mat = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+i = 0
+j = 0
+
+n = len(mat)
+m = len(mat[0])
+
+right = 1 # right vector
+down = 0 # down vector
+
+while i < n and j < m:
+    print(mat[i][j], end = " ")
+    j = j + right
+    right = -1 # change direction of vector
+    i = i + down
+    down = 1
+
+# down and left we can go only one step per iteration
+# diagonally up and down we can go until we reach the margin
