454e838e3306ac8c5ee5345b3e27347df0e065d7
Tudor Anghelina <16164903+tudoranghelina91@users.noreply.github.com>
2025-02-28 10:39:10 +0200

    Update spirally traverse

--- 5. Matrix/SpirallyTraverse.py
+++ 5. Matrix/SpirallyTraverse.py
@@ -4,29 +4,29 @@ def spirallyTraverse(mat):
     m = len(mat[0])
     
     istart = 0
-    iend = n - 1
+    iend = n
     
     jstart = 0
-    jend = m - 1
+    jend = m
     
     out = []
     
-    while istart <= iend and jstart <= jend:
-        for j in range(jstart, jend + 1):
+    while istart < iend and jstart < jend:
+        for j in range(jstart, jend):
             out.append(mat[istart][j])
         istart += 1
         
-        for i in range(istart, iend + 1):
-            out.append(mat[i][jend])
+        for i in range(istart, iend):
+            out.append(mat[i][jend - 1])
         jend -= 1
         
-        if istart <= iend:
-            for j in range(jend, jstart - 1, -1):
-                out.append(mat[iend][j])
+        if istart < iend:
+            for j in range(jend - 1, jstart - 1, -1):
+                out.append(mat[iend - 1][j])
             iend -= 1
 
-        if jstart <= jend:
-            for i in range(iend, istart - 1, -1):
+        if jstart < jend:
+            for i in range(iend - 1, istart - 1, -1):
                 out.append(mat[i][jstart])
             jstart += 1
         
