e470028be2cda576e0ce8923966e041d9701715e
Tudor Anghelina <16164903+tudoranghelina91@users.noreply.github.com>
2025-03-12 10:31:15 +0200

    Add arithmetic expression evaluation

---
+++ 6. Stack/ArithmeticExpressionEvaluation.py
@@ -0,0 +1,53 @@
+def infixToPostfix(postfix):
+    operatorStack = []
+    output = []
+
+    postfix = postfix.replace(" ", "")
+
+    precedence = {'^': 2, '*': 1, '/': 1, '+': 0, '-': 0}
+
+    for e in postfix:
+        if e.isalnum():
+            output.append(e)
+        elif e == '(':
+            operatorStack.append(e)
+        elif e == ')':
+            while operatorStack and operatorStack[-1] != '(':
+                output.append(operatorStack.pop())
+            operatorStack.pop()
+        else:
+            while operatorStack and operatorStack[-1] in precedence and precedence[operatorStack[-1]] >= precedence[e]:
+                output.append(operatorStack.pop())
+            operatorStack.append(e)
+    
+    while operatorStack:
+        output.append(operatorStack.pop())
+
+    return ''.join(output)
+
+def arithmeticExpressionEvaluation(infix):
+    postfix = infixToPostfix(infix)
+    stack = []
+    print(postfix)
+    for e in postfix:
+        if stack and e in ['^', '*', '/', '+', '-']:
+            a = int(stack.pop())
+            b = int(stack.pop())
+
+            if e == '^':
+                stack.append(a ** b)
+            elif e == '*':
+                stack.append(a * b)
+            elif e == '/':
+                stack.append(a // b)
+            elif e == '+':
+                stack.append(a + b)
+            elif e == '-':
+                stack.append(a - b)
+
+        else:
+            stack.append(e)
+    
+    return stack.pop()
+
+print(arithmeticExpressionEvaluation("(2+4) * (4+6)"))
