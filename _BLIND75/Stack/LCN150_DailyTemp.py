6c2028a0ad4e23de9acd7728b4f37488514724cd
Tudor Anghelina <16164903+tudoranghelina91@users.noreply.github.com>
2026-05-07 21:27:10 +0300

    Add LCN150_DailyTemp expanaition

--- _BLIND75/Stack/LCN150_DailyTemp.py
+++ _BLIND75/Stack/LCN150_DailyTemp.py
@@ -1,3 +1,17 @@
+"""
+The idea is to use a stack and iterate the array in a forward only manner.
+We will use a result array.
+
+In the stack we store the (index, temp) pair at every step
+
+When and while the current temp value is greater than the temp value on the stack we pop the index temperature pair out of the stack
+We store the difference between the current index and the pair index in the output stack at the pair index position
+
+(stackIndex, stackTemp) - pair to go on the stack
+currentIndex - stackIndex - difference to store at output[stackIndex]
+
+"""
+
 def dailyTemperatures(temperatures):
     res = [0] * len(temperatures)
     stack = []
